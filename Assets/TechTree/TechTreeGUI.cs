f929f0c33a899cf2ef1ddcd2470d1637e25e99c2
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-04-24 22:18:05 +0200

    change tech tree to automated align

---
+++ Assets/TechTree/TechTreeGUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class TechTreeGUI : MonoBehaviour
+{
+    public TechTreeNodeGraph techTree;
+    public GameObject nodePrefeb;
+    public GameObject tierPrefeb;
+
+    private Dictionary<int, GameObject> tier = new Dictionary<int, GameObject>();
+
+    private void Start()
+    {
+        foreach (TechTreeNode node in techTree.nodes)
+        {
+            if (GameObject.Find("tier " + node.tier) == null)
+            {
+                Debug.Log(tierPrefeb);
+                tier.Add(node.tier, Instantiate(tierPrefeb));
+                tier[node.tier].name = "tier " + node.tier;
+                tier[node.tier].transform.SetParent(transform);
+            }
+
+            GameObject newNode = Instantiate(nodePrefeb);
+            newNode.transform.SetParent(tier[node.tier].transform);
+            //newNode.transform.Find("Title").GetComponent<TMPro.TextMeshProUGUI>().text = node.title;
+/*            for (int i = 0; i <= node.childeren.Length; i++)
+            {
+                //ResearchButton newNode = Instantiate(nodePrefeb).AddComponent<ResearchButton>();
+                //newNode.Init(node);
+                //newNode.transform.parent = transform;
+                //newNode.transform.position = new Vector3(100, 100, 0);
+                // set node pos based on i
+            }*/
+        }
+    }
+}
---
+++ Assets/TechTree/TechTreeNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XNode;
+
+public class TechTreeNode : Node {
+
+	public string title;
+	public string description;
+	public Image image;
+	public int cost;
+	public int tier;
+
+	[Input] public TechTreeNode parent;
+	[Output] public TechTreeNode[] childeren;
+
+	// Use this for initialization
+	protected override void Init() {
+		base.Init();
+		// something new
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return 0f;
+	}
+}
