500713785a85e55ba5432948d5f26b695cbfee4e
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-05-12 14:24:37 +0200

    changed UPR

--- Assets/TechTree/TechTreeGUI.cs
+++ Assets/TechTree/TechTreeGUI.cs
@@ -9,7 +9,7 @@ public class TechTreeGUI : MonoBehaviour
     public GameObject nodePrefeb;
     public GameObject tierPrefeb;
 
-    private Dictionary<int, GameObject> tier = new Dictionary<int, GameObject>();
+    /*private Dictionary<int, GameObject> tier = new(); //?
 
     private void Start()
     {
@@ -22,18 +22,16 @@ public class TechTreeGUI : MonoBehaviour
                 tier[node.tier].name = "tier " + node.tier;
                 tier[node.tier].transform.SetParent(transform);
             }
-
-            GameObject newNode = Instantiate(nodePrefeb);
-            newNode.transform.SetParent(tier[node.tier].transform);
-            //newNode.transform.Find("Title").GetComponent<TMPro.TextMeshProUGUI>().text = node.title;
-/*            for (int i = 0; i <= node.childeren.Length; i++)
-            {
-                //ResearchButton newNode = Instantiate(nodePrefeb).AddComponent<ResearchButton>();
-                //newNode.Init(node);
-                //newNode.transform.parent = transform;
-                //newNode.transform.position = new Vector3(100, 100, 0);
-                // set node pos based on i
-            }*/
+        GameObject newNode = Instantiate(nodePrefeb);
+        newNode.transform.SetParent(tier[node.tier].transform);
         }
-    }
+
+        foreach (TechTreeNode node in techTree.nodes)
+        {
+            if (node.childeren.Length > 0)
+            {
+
+            }
+        }
+    }*/
 }
