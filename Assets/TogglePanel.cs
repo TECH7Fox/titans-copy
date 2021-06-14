c49c16c7e50012853b3c828638c6f792d5075d2f
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-06-15 01:27:03 +0200

    add layout for hotbar and building panel

---
+++ Assets/TogglePanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePanel : MonoBehaviour
+{
+    public void togglePanel()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
