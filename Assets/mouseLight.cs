792d69a38561e20d3e3dca5ea8c2fd20023d1d30
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-06-18 04:22:19 +0200

    Add light to cursor

---
+++ Assets/mouseLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLight : MonoBehaviour
+{
+    public Transform cursorLight;
+    public float distance = 500f;
+    public LayerMask layerMask;
+    public float distanceFromHit = 10;
+    public Transform planet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, distance, layerMask))
+        {
+            cursorLight.position = hitInfo.point + (hitInfo.point - planet.position).normalized * distanceFromHit;            
+        } else
+        {
+            cursorLight.position = Vector3.zero;
+        }
+    }
+}
